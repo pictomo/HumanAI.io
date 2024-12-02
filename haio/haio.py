100bbce697a9f41a77d3a82304df3499004424b4
pictomo <tomo.pict@gmail.com>
2024-12-02 23:56:40 +0900

    [update]bs4を用いてテンプレートを操作

--- haio/haio.py
+++ haio/haio.py
@@ -51,53 +51,56 @@ class OpenAI_IO:
         return self.get_answer()
 
 
-template1: str = """
-<HTMLQuestion xmlns="http://mechanicalturk.amazonaws.com/AWSMechanicalTurkDataSchemas/2011-11-11/HTMLQuestion.xsd">
-    <HTMLContent><![CDATA[
+# template1: str = """
+# <HTMLQuestion xmlns="http://mechanicalturk.amazonaws.com/AWSMechanicalTurkDataSchemas/2011-11-11/HTMLQuestion.xsd">
+#     <HTMLContent><![CDATA[
 
-    <!DOCTYPE html>
-    <html>
-        <head>
-            <meta http-equiv='Content-Type' content='text/html; charset=UTF-8'/>
-            <script type='text/javascript' src='https://s3.amazonaws.com/mturk-public/externalHIT_v1.js'></script>
-        </head>
-        <body>
-            <center>
-                <form name='mturk_form' method='post' id='mturk_form' action='https://www.mturk.com/mturk/externalSubmit'>
-                    <h2>{question}</h2>
-                    <p><input type='text' name='response'/></p>
-                    <p><input type='submit' id='submitButton' value='Submit' /></p>
-                    <input type='hidden' value='' name='assignmentId' id='assignmentId'/>
-                </form>
-            </center>
-            <script language='Javascript'>turkSetAssignmentID();</script>
-        </body>
-    </html>
+#     <!DOCTYPE html>
+#     <html>
+#         <head>
+#             <meta http-equiv='Content-Type' content='text/html; charset=UTF-8'/>
+#             <script type='text/javascript' src='https://s3.amazonaws.com/mturk-public/externalHIT_v1.js'></script>
+#         </head>
+#         <body>
+#             <center>
+#                 <form name='mturk_form' method='post' id='mturk_form' action='https://www.mturk.com/mturk/externalSubmit'>
+#                     <h2>{question}</h2>
+#                     <p><input type='text' name='response'/></p>
+#                     <p><input type='submit' id='submitButton' value='Submit' /></p>
+#                     <input type='hidden' value='' name='assignmentId' id='assignmentId'/>
+#                 </form>
+#             </center>
+#             <script language='Javascript'>turkSetAssignmentID();</script>
+#         </body>
+#     </html>
 
-    ]]>
-    </HTMLContent>
-    <FrameHeight>800</FrameHeight>
-</HTMLQuestion>
-"""
+#     ]]>
+#     </HTMLContent>
+#     <FrameHeight>800</FrameHeight>
+# </HTMLQuestion>
+# """
 
 # https://requester.mturk.com/create/projects/new collect utterance
-template2: str = """
+outer_template: str = """
 <HTMLQuestion xmlns="http://mechanicalturk.amazonaws.com/AWSMechanicalTurkDataSchemas/2011-11-11/HTMLQuestion.xsd">
     <HTMLContent><![CDATA[
-        <!DOCTYPE html>
-            <body>
-                <script src="https://assets.crowd.aws/crowd-html-elements.js"></script>
-                <crowd-form answer-format="flatten-objects">
-
-                    <h2>{question}</h2>
-
-                    <crowd-input name="response" placeholder="Type your answer here..." required></crowd-input>
-                </crowd-form>
-            </body>
-        </html>
+        {inner_template}
     ]]></HTMLContent>
     <FrameHeight>0</FrameHeight>
 </HTMLQuestion>
+"""
+inner_template1: str = """
+<!DOCTYPE html>
+    <body>
+        <script src="https://assets.crowd.aws/crowd-html-elements.js"></script>
+        <crowd-form answer-format="flatten-objects">
+
+            <!-- question here -->
+
+            <crowd-input name="response" placeholder="Type your answer here..." required></crowd-input>
+        </crowd-form>
+    </body>
+</html>
 """
 
 
@@ -118,6 +121,11 @@ class MTurk_IO:
         if self.hit_id != "":
             raise Exception("already asking")
 
+        soup = BeautifulSoup(inner_template1, "html.parser")
+        new_h2 = soup.new_tag("h2")
+        new_h2.string = question
+        soup.find("crowd-form").insert(0, new_h2)
+
         res = self.mturk_client.create_hit(
             Title="Answer a question",
             Description="Please answer the following question simply and accurately.",
@@ -126,8 +134,8 @@ class MTurk_IO:
             MaxAssignments=1,  # 受け付ける回答数（＝ワーカー数）上限
             LifetimeInSeconds=3600,  # HITの有効期限を3600秒（1時間）後に設定
             AssignmentDurationInSeconds=300,  # HITの制限時間を300秒（5分）に設定
-            Question=template2.format(
-                question=question
+            Question=outer_template.format(
+                inner_template=soup.prettify()
             ),  # my_hit.htmlの中身を文字列でそのまま渡す
         )
         print("HIT ID:", res["HIT"]["HITId"])
